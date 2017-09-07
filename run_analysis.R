963183654d12b6fe614738862d7684e8c6ea787f
thisjayanderson <jay.m.anderson@gmail.com>
2017-09-07 17:31:00 -0400

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,24 @@
+# Anderson
+# run_analysis.R
+#
+# Getting and Cleaning Data Course Project
+#
+# Purpose:
+# 1) Merges the training and the test sets to create one data set.
+# 2) Extracts only the measurements on the mean and standard deviation for 
+#     each measurement.
+# 3) Uses descriptive activity names to name the activities in the data set.
+# 4) Appropriately labels the data set with descriptive variable names.
+# 5) From the data set in step 4, creates a second, independent tidy data 
+#     set with the average of each variable for each activity and each subject.
+################################################################################
+# Load dplyr package for data manipulation
+library(dplyr)
+
+# Download and read in the data sets
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
+download.file(url, "data.zip", method = "curl")
+
+# Merge the data sets into one
+
+
